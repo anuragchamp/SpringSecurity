092844d46a5abff9de0352bcc1aead564e03d3cb
anurag chhaperwal <anuragchamp71@gmail.com>
2021-07-26 05:19:18 +0530

    sign up

---
+++ src/main/java/com/users/dao/UserRepo.java
@@ -0,0 +1,12 @@
+package com.users.dao;
+
+import com.security.dto.SignupDto;
+
+public class UserRepo implements UsersDao {
+
+	public void save(SignupDto signupDto) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/users/dao/UsersDao.java
@@ -0,0 +1,8 @@
+package com.users.dao;
+
+import com.security.dto.SignupDto;
+
+public interface UsersDao {
+
+	void save(SignupDto signupDto);
+}
